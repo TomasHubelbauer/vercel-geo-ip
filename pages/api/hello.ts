4e6cf1864612e729c22b77fbf0adf743c3684377
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-01-27 23:51:51 +0100

    Look up geo IP headers in rawHeaders and log them
    I checked the serverless function logs and it seems as though the structure of the rawHeaders array is an alternation of keys and values. I look up the indices of the keys of interest and pull the corresponding values shifted by one. I also log request.headers in case the typings do not show the members for these headers but they are available at runtime. If I rule this out, I will stick with the above, but if I find members for them, I will rewrite the code to treat request.headers as any and pull the respective members from that.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -7,10 +7,10 @@ const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env
 type Data = { name: string };
 
 export default function handler(request: NextApiRequest, response: NextApiResponse<Data>) {
-  const country = request.rawHeaders;
-  const region = request.rawHeaders;
-  const city = request.rawHeaders;
-  console.log(request.rawHeaders);
+  const country = request.rawHeaders[request.rawHeaders.indexOf('x-vercel-ip-country') + 1];
+  const region = request.rawHeaders[request.rawHeaders.indexOf('x-vercel-ip-country-region') + 1];
+  const city = request.rawHeaders[request.rawHeaders.indexOf('x-vercel-ip-city') + 1];
+  console.log({ country, region, city, headers: request.headers });
   supabase.from('logs').insert({ text: 'API hit', url: request.url }).then(console.log);
   console.log('API hit', request.url);
   response.status(200).json({ name: 'John Doe' })
